e6a2148dce3e0881648869a0c02617326c533351
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-02-18 14:33:40 -0800

    toggles AirBnbListingsDuplicatePipeLine is settings.py

--- ingestion/airbnb_scrapping/airbnb_listings/settings.py
+++ ingestion/airbnb_scrapping/airbnb_listings/settings.py
@@ -11,7 +11,6 @@ BOT_NAME = "airbnb_listings"
 SPIDER_MODULES = ["airbnb_listings.spiders"]
 NEWSPIDER_MODULE = "airbnb_listings.spiders"
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = "airbnb_listings (+http://www.yourdomain.com)"
 
@@ -61,9 +60,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "airbnb_listings.pipelines.AirbnbListingsPipeline": 300,
-# }
+ITEM_PIPELINES = {
+    "airbnb_listings.pipelines.AirBnbListingsDuplicatePipeLine": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
