bcaeab127ca2450ff8ffaeedb1668a8f20a41db3
PooriaT <pooria_taghdiri@hotmail.com>
2024-02-22 23:57:47 -0800

    Unittest updated

--- analytics/policy/tests/unit/test_registration_number_policies.py
+++ analytics/policy/tests/unit/test_registration_number_policies.py
@@ -28,31 +28,24 @@ def test_invalid_pattern():
         assert registration_number_policies.valid_registration_number_policy() is False
 
 def test_unique_registration_number_policy():
-    registration_number = "24-160188"
-    registration_number_policies = RegistrationNumberPolicies(registration_number)
-    assert registration_number_policies.unique_registration_number_policy() is True
+    registration_numbers = {
+        "24-160188": True,
+        "20-160574": False,
+        "": False,
+        None: False
+    }
 
-    registration_numbers = [
-        "20-160574",
-        "",
-        None
-    ]
-
-    for registration_number in registration_numbers:
-        registration_number_policies = RegistrationNumberPolicies(registration_number)
-        assert registration_number_policies.unique_registration_number_policy() is False
+    for key, value in registration_numbers.items():
+        registration_number_policies = RegistrationNumberPolicies(key)
+        assert registration_number_policies.unique_registration_number_policy() is value
 
 def test_existed_registration_number_policy():
+    registration_numbers = {
+        "24-160188": True,
+        "": False,
+        None: False
+    }
 
-    registration_number = "24-160188"
-    registration_number_policies = RegistrationNumberPolicies(registration_number)
-    assert registration_number_policies.existed_registration_number_policy() is True
-
-    registration_numbers = [
-        "",
-        None
-    ]
-
-    for registration_number in registration_numbers:
-        registration_number_policies = RegistrationNumberPolicies(registration_number)
-        assert registration_number_policies.unique_registration_number_policy() is False
+    for key, value in registration_numbers.items():
+        registration_number_policies = RegistrationNumberPolicies(key)
+        assert registration_number_policies.unique_registration_number_policy() is value
